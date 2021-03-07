c9b89e2e3c243f89f7fc129563131a9716d54ebc
José Junior <200221001@estudantes.ips.pt>
2021-03-07 20:51:27 -0300

    Saída do programa.

---
+++ AppStart.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class AppStart here.
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class AppStart
+{    
+    Name         :  MEDIUM
+    toString()   : "Médio"
+    Ordem        : 1
+    Valor minimo : 37
+    Valor maximo : 44
+    Código       : 'M'
+}
+
